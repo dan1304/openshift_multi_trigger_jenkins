c21f344a576de4d8d3e8f16160c9a99ca94e6733
Tuyen TD <tuyentd@yandex.com>
2022-04-15 15:42:12 +0700

    add shellUtils for preparing .env

--- shellUtils.sh
+++ shellUtils.sh
@@ -1,3 +1,8 @@
+#!/bin/bash
 # replace with your namespace to get correct token
-OCP_JENKINS_SECRET=$(oc get secret -n #Your_Namespace# | grep jenkins-token  | awk 'NR==1{print $1; exit}')
-OCP_JENKINS_TOKEN=$(oc get secret -n #Your_Namespace# ${OCP_JENKINS_SECRET} -o=jsonpath={.data.token} | base64 -D)
+OCP_JENKINS_URL="https://jenkins-equator.cicd.mn1.ocp.ascendmoney-dev.internal/job/equator-cicd/job/equator-cicd-"
+OCP_JENKINS_SECRET=$(oc get secret -n equator-cicd | grep jenkins-token  | awk 'NR==1{print $1; exit}')
+OCP_JENKINS_TOKEN=$(oc get secret -n equator-cicd ${OCP_JENKINS_SECRET} -o=jsonpath={.data.token} | base64 -D)
+
+echo "OCP_JENKINS_URL=\"${OCP_JENKINS_URL}"\" > ./.env
+echo "OCP_JENKINS_TOKEN=\"${OCP_JENKINS_TOKEN}"\" >> ./.env
